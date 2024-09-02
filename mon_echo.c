b5506e4e7fbcb77687937e0ee71118cca756dd35
UnelDev <enee.prog@gmail.com>
2024-09-02 15:37:41 +0200

    add mon_echo.c

--- mon_echo.c
+++ mon_echo.c
@@ -1,9 +1,18 @@
 /* à compléter: entêtes */
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	/* à compléter: corps de main */
+
+	for (int i = 1; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+		if (argc != i + 1)
+		{
+			printf(" ");
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
