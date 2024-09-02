47669e20b5e129d8a19fc75f68517c48c3e7e7e3
UnelDev <enee.prog@gmail.com>
2024-09-02 14:48:57 +0200

    add produit.c

--- produit.c
+++ produit.c
@@ -1,9 +1,14 @@
-/* à compléter: entêtes */
+#include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-	/* à compléter: corps de main */
-
+	int first, second;
+	printf("programme qui multiplie 2 nombres\n");
+	printf("saisir un premier nombre : ");
+	scanf("%d", &first);
+	printf("saisir un deuxieme nombre : ");
+	scanf("%d", &second);
+	printf("%d x %d = %d\n", first, second, (first * second));
 	return EXIT_SUCCESS;
 }
