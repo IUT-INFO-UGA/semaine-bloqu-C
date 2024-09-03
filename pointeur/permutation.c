4660c4aea3e919b908cabc0f6053a322737c5380
UnelDev <enee.prog@gmail.com>
2024-09-03 10:44:34 +0200

    add pointeur/permutation.c

---
+++ pointeur/permutation.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void permuter(int *adr_a, int *adr_b)
+{
+	int temp = *adr_a;
+	*adr_a = *adr_b;
+	*adr_b = temp;
+}
+
+int main(void)
+{
+	int val1 = 1,
+		val2 = 2;
+	printf("val1: %d, val2: %d\n", val1, val2);
+	permuter(&val1, &val2);
+	printf("val1: %d, val2: %d\n", val1, val2);
+}
