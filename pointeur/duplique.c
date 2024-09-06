d5df4ab2f1f57de814a429f3290eb7d107a297e7
UnelDev <enee.prog@gmail.com>
2024-09-06 10:22:42 +0200

    update pointeur/duplique.c

--- pointeur/duplique.c
+++ pointeur/duplique.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-char *copier_chaine(const char *str)
+void my_strcpy(char *out, char *in)
 {
-	const int size = strlen(str);
-	char *copy = malloc(sizeof(char) + 1);
+	int i = 0;
+	while (in[i] != '\0')
+	{
+		out[i] = in[i];
+		i++;
+	}
+
+	out[i] = '\0';
+}
+
+int my_strlen(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+		i++;
+	return i;
+}
+
+char *copier_chaine(char *str)
+{
+	char *copy = malloc(sizeof(char) * my_strlen(str) + 1);
 	if (copy == NULL)
 		return "\0";
-	strcpy(copy, str);
+	my_strcpy(copy, str);
 	return copy;
 }
 
 int main(void)
 {
-	const char *from = "bonjour";
+	char *from = "bonjour";
 	char *to = copier_chaine(from);
 	printf("from: %s\nto %s", from, to);
 	free(to);
